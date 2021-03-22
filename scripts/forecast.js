1eb3a9620fe3a87c5d504ddb758c64019e16fa71
Cole Robinson <2colewestin@gmail.com>
2021-03-21 19:07:17 -0500

    To retrieve json data
    added a getWeather and getCity async to return an object with the city and current weather. This is converted into actual output in app.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,33 @@
 const key = 'XgqArYZ3iNrupYWsk5lPnggcbchT2oLy';
+
+//get weather info
+const getWeather = async (id) =>{
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data [0];
+
+};
+
+
+//get city info
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data [0];
+};
+
+// getCity('tokyo').then(data => {
+//     return getWeather (data.Key);
+// }).then(data=> {
+//     console.log(data);
+// }).catch (err => console.log(err));
+
