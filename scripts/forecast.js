a8dcbaa09423bb5e7d5d41eb935bf4f3bd6505a4
Cole Robinson <CWRobinson@gmx.com>
2021-03-31 18:29:00 -0500

    added class constructor
    Formatted to use the Forecast class to get some practice with object oriented coding.

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,33 +1,32 @@
-const key = 'cfE5nvD8BA3AJLN5EB4DcS6x3V50XAbb';
-
-//get weather info
-const getWeather = async (id) =>{
-
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data [0];
-
+class Forecast{
+    constructor(){
+        this.key = 'cfE5nvD8BA3AJLN5EB4DcS6x3V50XAbb';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return {
+            //object shorthand notation
+            cityDets,
+            weather
+        };
+    }
+    async getCity(city){
+        // const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data [0];
+    }
+    async getWeather(id){
+        //const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data [0];
+    
+    };
 };
-
-
-//get city info
-const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data [0];
-};
-
-// getCity('tokyo').then(data => {
-//     return getWeather (data.Key);
-// }).then(data=> {
-//     console.log(data);
-// }).catch (err => console.log(err));
-
+// Below code replaced by class constructors in 3/31/21 update
